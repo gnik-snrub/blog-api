7cbc501777dcd59655ebbd3cd3666aa7a2a925be
Josiah Morris <josiahbmorris@hotmail.com>
2023-10-02 02:44:50 +0900

    Connected to MongoDB collection

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
+const mongoose = require('mongoose')
 const express = require('express')
 const apiRouter = require('./routes/api')
 
 require('dotenv').config()
 
+mongoose.set('strictQuery', false)
+const mongoDB = process.env.MONGO_CT_URL
+
+main().catch((err) => {console.log(err)})
+async function main() {
+  await mongoose.connect(mongoDB)
+}
+
 const app = express()
 
 app.use('/api', apiRouter)
-
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
 
 app.listen(process.env.PORT, () => console.log(`Server started on port ${process.env.PORT}`))
