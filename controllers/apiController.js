fdad8bce60789f183487fa7a29a8f250ece7a6a3
Josiah Morris <josiahbmorris@hotmail.com>
2023-10-02 03:39:00 +0900

    Added controls for createPost

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,8 +1,18 @@
+const Post = require('../models/post')
 
-exports.createPost = async(req, res) => {}
 exports.readPost = (req, res) => {}
 exports.updatePost = async(req, res) => {}
 exports.deletePost = async(req, res) => {}
+exports.createPost = async(req, res) => {
+  const newPost = new Post({
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    isPublished: false
+  })
+  await newPost.save()
+  res.send(`${newPost} has been submitted`)
+}
 
 exports.createComment = async(req, res) => {}
 exports.readComment = (req, res) => {}
