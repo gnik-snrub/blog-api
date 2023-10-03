286783a32b3a1fc15ff8bebd7eebba387916dff2
Josiah Morris <josiahbmorris@hotmail.com>
2023-10-04 03:45:40 +0900

    Added verifyToken middleware using JWT

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const express = require('express')
 const apiRouter = require('./routes/api')
 
+const jwt = require('jsonwebtoken')
 const bodyParser = require('body-parser')
 
 require('dotenv').config()
@@ -18,6 +19,18 @@ const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
+
+function verifyToken (req, res, next) {
+  try {
+    const token = req.headers.authorization.split(' ')[1]
+    const verified = jwt.verify(token, process.env.JWT_SECRET_KEY)
+    req.user = verified
+    next()
+  } catch(err) {
+    res.status(400).send('Invalid token')
+  }
+}
+app.use(verifyToken)
 
 app.use('/api', apiRouter)
 
