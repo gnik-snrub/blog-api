7617301aa2af06bdf1cb80767098d6653c6be4c4
Josiah Morris <josiahbmorris@hotmail.com>
2023-10-07 01:38:56 +0900

    Moved verifyToken function to api route

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const apiController = require('./controllers/apiController')
+
 const mongoose = require('mongoose')
 const express = require('express')
 const apiRouter = require('./routes/api')
@@ -20,18 +22,11 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
 
-function verifyToken (req, res, next) {
-  try {
-    const token = req.headers.authorization.split(' ')[1]
-    const verified = jwt.verify(token, process.env.JWT_SECRET_KEY)
-    req.user = verified
-    next()
-  } catch(err) {
-    res.status(400).send('Invalid token')
-  }
-}
-app.use(verifyToken)
+const cors = require('cors')
+app.use(cors())
 
 app.use('/api', apiRouter)
+
+app.use('/', express.static('frontend/dist'))
 
 app.listen(process.env.PORT, () => console.log(`Server started on port ${process.env.PORT}`))
--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,25 @@ const router = express.Router()
 
 const apiController = require('../controllers/apiController')
 
+function verifyToken (req, res, next) {
+  try {
+    const token = req.headers.authorization.split(' ')[1]
+    const verified = jwt.verify(token, process.env.JWT_SECRET_KEY)
+    req.user = verified
+    next()
+  } catch(err) {
+    res.status(400).send('Invalid token')
+  }
+}
+
+router.use((req, res, next) => {
+  if (req.method !== 'GET' && req.path !== '/login') {
+    verifyToken(req, res, next)
+  } else {
+    next()
+  }
+})
+
 router.get('/posts', apiController.readPosts)
 router.post('/posts', apiController.createPost)
 router.get('/posts/:postID', apiController.readPost)
