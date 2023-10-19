d0eef18b438e517a2ee5ab36cafa4ef206a8b23c
Josiah Morris <josiahbmorris@hotmail.com>
2023-10-20 05:17:34 +0900

    Added data fetching load function to /admin route

---
+++ admin/src/routes/admin/+page.js
@@ -0,0 +1,11 @@
+export async function load () {
+  const postsResponse = await fetch(`${import.meta.env.VITE_API_DOMAIN}/api/posts/`)
+  const posts = await postsResponse.json()
+  let comments = []
+  for (let i = 0; i < posts.length; i++) {
+    const postCommentsResponse = await fetch(`${import.meta.env.VITE_API_DOMAIN}/api/posts/${posts[i]._id}/comments`)
+    const postComments = await postCommentsResponse.json()
+    comments = [...comments, ...postComments]
+  }
+  return { posts, comments }
+}
---
+++ admin/src/routes/admin/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let data
+  const { posts, comments } = data
+  let postCount = posts.length
+  let averageComments = comments.length / postCount
+</script>
+
+<p>There are {postCount} posts with an average of {averageComments} comments per post!</p>
