59c2886e67005c26c93f3afe191f97cf8e0027a1
Josiah Morris <josiahbmorris@hotmail.com>
2023-10-21 05:32:48 +0900

    Added load function to view_unpublished route to fetch unpublished posts

--- admin/src/routes/admin/view_unpublished/+page.svelte
+++ admin/src/routes/admin/view_unpublished/+page.svelte
@@ -1,1 +1,9 @@
+<script>
+  export let data
+  const { filtered } = data
+</script>
 <p>All unpublished posts!</p>
+
+{#each filtered as post }
+  <a href={`/admin/${post._id}`}><p>{post.title}</p></a>
+{/each}
