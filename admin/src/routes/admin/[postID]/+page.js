c8f7bfab94175712b75a99385e3c16bcfb56707f
Josiah Morris <josiahbmorris@hotmail.com>
2023-10-21 05:33:30 +0900

    Added admin/[postID] route to view specific posts

---
+++ admin/src/routes/admin/[postID]/+page.js
@@ -0,0 +1,7 @@
+export async function load({ params }) {
+  const postResponse = await fetch(`${import.meta.env.VITE_API_DOMAIN}/api/posts/${params.postID}`)
+  const postData = await postResponse.json()
+  const commentsResponse = await fetch(`${import.meta.env.VITE_API_DOMAIN}/api/posts/${params.postID}/comments`)
+  const commentsData = await commentsResponse.json()
+  return { post: postData, comments: commentsData, postID: params.postID }
+}
---
+++ admin/src/routes/admin/[postID]/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let data
+  const { post, comments, postID } = data
+</script>
+
+<h1>{postID}</h1>
+<div>{JSON.stringify(post)}</div>
+  <div>{JSON.stringify(comments)}</div>
