22951c8fd03a3aa2473d5188d15bd547396d1c88
Josiah Morris <josiahbmorris@hotmail.com>
2023-10-23 05:41:01 +0900

    Built new post form at admin/new_post route

--- admin/src/routes/admin/new_post/+page.svelte
+++ admin/src/routes/admin/new_post/+page.svelte
@@ -1,1 +1,30 @@
 <p>New Post!</p>
+<script>
+  let title, author, content = ''
+  let isPublished = false
+  async function submitPost() {
+  function togglePublished() {
+  }
+</script>
+
+<h2>New Post!</h2>
+
+<form on:submit|preventDefault={submitPost}>
+  <label>
+    Title
+    <input bind:value={title} type='text'/>
+  </label>
+  <label>
+    Author
+    <input bind:value={author} type='text'/>
+  </label>
+  <label>
+    Content
+    <input bind:value={content} type='textarea'/>
+  </label>
+  <label>
+    Publish on submit?
+    <input on:change={togglePublished} type='checkbox'/>
+  </label>
+  <button type="submit">Submit Post</button>
+</form>
