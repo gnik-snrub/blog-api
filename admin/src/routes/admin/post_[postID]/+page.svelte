88f3d8e3e87c580f8157ca2ef05c6bc73e155aa5
Josiah Morris <josiahbmorris@hotmail.com>
2023-10-23 03:55:09 +0900

    Added initial semantics and styling to post_[postID] route

--- admin/src/routes/admin/post_[postID]/+page.svelte
+++ admin/src/routes/admin/post_[postID]/+page.svelte
@@ -2,3 +2,27 @@
   export let data
   const { post, comments } = data
 </script>
+
+<h2>{post.title}</h2>
+<p>{post.date_yyyy_mm_dd} - {post.author}</p>
+<p>{post.isPublished ? 'Post has been published' : 'Post is currently unpublished'}</p>
+<p>{post.content}</p>
+<h3>Comments: </h3>
+<ul>
+{#each comments as comment}
+  <li>
+    <p>User: {comment.username}</p>
+    <p>Timestamp: {comment.timestamp_formatted}</p>
+    <p>Content: {comment.content}</p>
+  </li>
+{/each}
+</ul>
+
+<style>
+  p {
+    margin: 0;
+  }
+  li {
+    margin-bottom: 1em;
+  }
+</style>
