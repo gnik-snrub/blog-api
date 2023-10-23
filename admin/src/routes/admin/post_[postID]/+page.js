596fb50b7ffb531b6253d30624f0a64387da9503
Josiah Morris <josiahbmorris@hotmail.com>
2023-10-24 02:59:18 +0900

    Changed all pages to use fetch function passed into load

--- admin/src/routes/admin/post_[postID]/+page.js
+++ admin/src/routes/admin/post_[postID]/+page.js
@@ -1,4 +1,4 @@
-export async function load({ params }) {
+export async function load({ fetch, params }) {
   const postResponse = await fetch(`${import.meta.env.VITE_API_DOMAIN}/api/posts/${params.postID}`)
   const postData = await postResponse.json()
   const commentsResponse = await fetch(`${import.meta.env.VITE_API_DOMAIN}/api/posts/${params.postID}/comments`)
--- admin/src/routes/admin/view_published/+page.js
+++ admin/src/routes/admin/view_published/+page.js
@@ -1,4 +1,4 @@
-export async function load() {
+export async function load({ fetch }) {
   const apiResponse = await fetch(`${import.meta.env.VITE_API_DOMAIN}/api/posts/`)
   const data = await apiResponse.json()
   const filtered = data.filter(post => post.isPublished)
