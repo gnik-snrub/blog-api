3e7f5509a9bba0f454bff5e2358e7d23f2e9f4db
Josiah Morris <josiahbmorris@hotmail.com>
2023-10-24 04:04:20 +0900

    Added delete buttons to view_published list

--- admin/src/routes/admin/view_published/+page.svelte
+++ admin/src/routes/admin/view_published/+page.svelte
@@ -1,10 +1,52 @@
 <script>
   export let data
   const { filtered } = data
+
+  let items = filtered.map((item) => {
+    return {item, showDeleteConfirmation: false}
+  })
+
+  function toggleDeleteConfirm(id) {
+    items = items.map((item) => {
+      return item.item._id === id ? {...item, showDeleteConfirmation: !item.showDeleteConfirmation} : item
+    })
+  }
+
+  async function deletePost(id) {
+    await fetch(`${import.meta.env.VITE_API_DOMAIN}/api/posts/${id}`, {
+      method: 'DELETE'
+    })
+    location.reload()
+  }
 </script>
 
 <p>All published posts!</p>
 
-{#each filtered as post }
-  <a href={`/admin/post_${post._id}`}><p>{post.title}</p></a>
+<ul>
+{#each items as {item, showDeleteConfirmation} }
+  <li>
+    {#if !showDeleteConfirmation}
+      <button on:click={() => toggleDeleteConfirm(item._id)}>Delete</button>
+      <a href={`/admin/post_${item._id}`}><p>{item.title} - {showDeleteConfirmation}</p></a>
+    {:else}
+      <button on:click={() => deletePost(item._id)}>Delete</button>
+      <p>Are you sure you want to delete '{item.title}'?</p>
+      <button on:click={() => toggleDeleteConfirm(item._id)}>Cancel</button>
+    {/if}
+  </li>
 {/each}
+</ul>
+
+<style>
+  li {
+    width: 100%;
+    display: grid;
+    grid-template-columns: auto max-content auto;
+    align-items: center;
+    justify-content: start;
+  }
+  button {
+    height: fit-content;
+    margin: 0 2em;
+  }
+</style>
