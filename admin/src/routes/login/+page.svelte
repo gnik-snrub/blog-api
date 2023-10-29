abbc2ca34b4b42c767e3585bec52b910f19aeb31
Josiah Morris <josiahbmorris@hotmail.com>
2023-10-30 05:30:52 +0900

    Created layout for login +page.svelte

---
+++ admin/src/routes/login/+page.svelte
@@ -0,0 +1,13 @@
+<p>Login page!</p>
+
+<form on:submit|preventDefault={handleSubmit}>
+  <label>
+    Username:
+    <input bind:value={username} type="text" />
+  </label>
+  <label>
+    Password:
+    <input bind:value={password} type="password" />
+  </label>
+  <button type="submit">Login</button>
+</form>
