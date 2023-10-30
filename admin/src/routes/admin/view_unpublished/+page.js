cb8831626119378626409189f1beb6aa0331e1e5
Josiah Morris <josiahbmorris@hotmail.com>
2023-10-31 03:20:56 +0900

    Deleted now redundant +page.js files from view_published and view_unpublished routes

--- admin/src/routes/admin/view_unpublished/+page.js
+++
@@ -1,6 +0,0 @@
-export async function load({ fetch }) {
-  const apiResponse = await fetch(`${import.meta.env.VITE_API_DOMAIN}/api/posts/`)
-  const data = await apiResponse.json()
-  const filtered = data.filter(post => !post.isPublished)
-  return {filtered}
-}
