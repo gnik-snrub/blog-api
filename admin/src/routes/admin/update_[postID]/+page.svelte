898c5cdf8694d872cac3d96e57f84cc7bcadb324
Josiah Morris <josiahbmorris@hotmail.com>
2023-11-01 04:11:50 +0900

    Added title and update form to update_[postID] route

---
+++ admin/src/routes/admin/update_[postID]/+page.svelte
@@ -0,0 +1,21 @@
+<h2>Update Post!</h2>
+
+<form on:submit|preventDefault={submitPost}>
+  <label>
+    Title
+    <input bind:value={title} type='text'/>
+  </label>
+  <label>
+    Author
+    <input bind:value={author} type='text'/>
+  </label>
+  <label>
+    Content
+    <input bind:value={content} type='textarea'/>
+  </label>
+  <label>
+    Publish on submit?
+    <input bind:checked={isPublished} onclick={togglePublished} type='checkbox'/>
+  </label>
+  <button type="submit">Update Post</button>
+</form>
