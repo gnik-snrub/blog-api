a13c74156d491ac5e0335146ae5c12efa6c5c017
Josiah Morris <josiahbmorris@hotmail.com>
2023-11-07 02:38:57 +0900

    Added commentData, postID, and commentID to return for +page.js load function

--- admin/src/routes/admin/post_[postID]/update_[commentID]/+page.js
+++ admin/src/routes/admin/post_[postID]/update_[commentID]/+page.js
@@ -1,1 +1,5 @@
-
+export async function load({ fetch, params }) {
+  const commentResponse = await fetch(`${import.meta.env.VITE_API_DOMAIN}/api/posts/${params.postID}/comments/${params.commentID}`)
+  const commentData = await commentResponse.json()
+  return { comment: commentData, postID: params.postID, commentID: params.commentID }
+}
